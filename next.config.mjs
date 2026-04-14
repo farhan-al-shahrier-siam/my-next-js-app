6e88eaac1137890bb55adaf8c8b8de7ffe3630db
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-04-14 22:53:52 +0600

    Exploring Image tag in as it makes any image in the website more optimized

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,18 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      // https://i.ibb.co.com/8LfLDby4/images-3.jpg
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com',
+        port: '',
+        pathname: '/8LfLDby4/images-3.jpg',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
+import spider_man from "../../assets/images/spiderman.jpeg"
 
 const AboutPage = () => {
     return (
         <div>
             <h2>This is about page</h2>
+            <Image height="400" width="500" src="/pikachu.jpg" alt="an image of pikachu"></Image>
+            <Image height="400" width="500" src={spider_man} alt="an image of spider-man"></Image>
+            <Image height="400" width="500" src="https://i.ibb.co.com/8LfLDby4/images-3.jpg" alt="an image of spider-man"></Image>
+
         </div>
     );
 };
